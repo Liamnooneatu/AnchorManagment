738f42f8db11dcf2e1ba6f0f230a24a2fdfdde7e
NAOISE O LOUGHLIN - STUDENT <g00385498@atu.ie>
2025-12-02 14:39:01 +0000

    Initial Project Structure

---
+++ next/fullStackWeek11lab2/components/generic/HamMenu.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { IoIosMenu } from 'react-icons/io';
+import classes from "./HamMenu.module.css";
+
+export default function ShopDropdown() {
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+
+  function handleSelect(category) {
+    router.push('/shop/' + category);
+    setOpen(false); // close list after selection
+  }
+
+  return (
+    <div className={classes.mainDiv}>
+      {/* Icon button */}
+      <span
+        className={classes.mainSpan}
+        onClick={() => setOpen(prev => !prev)}
+      >
+        <IoIosMenu />
+      </span>
+
+      {/* Pop‑out list */}
+      {open && (
+        <ul className={classes.dropdownList}>
+          <li onClick={() => handleSelect('Boats')}>Boats</li>
+          <li onClick={() => handleSelect('Parts')}>Parts</li>
+          <li onClick={() => handleSelect('Engines')}>Engines</li>
+          <li onClick={() => handleSelect('Second-Hand')}>Second-Hand</li>
+        </ul>
+      )}
+    </div>
+  );
+}
+
+
